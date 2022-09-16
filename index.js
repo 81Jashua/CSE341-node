11064e3d233299893cd388fc634fec344f84ca5e
Jashua <77981430+81Jashua@users.noreply.github.com>
2022-09-16 13:43:50 -0600

    update

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Joshua Snyder')
 })
 
-// app.listen(3000, () => {
-//     console.log("application listening on http:/localhost:3000");
-// });
+app.listen(3000, () => {
+    console.log("application listening on http://localhost:3000");
+});
 
-app.listen(3000);
+// app.listen(3000);
