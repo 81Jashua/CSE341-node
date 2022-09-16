c1326fbd4a6960f12ab4c8ec590d5b4a65456b92
Jashua <77981430+81Jashua@users.noreply.github.com>
2022-09-16 10:46:55 -0600

    adding nodemon

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+// app.listen(3000, () => {
+//     console.log("application listening on http:/localhost:3000");
+// });
+
+app.listen(3000);
