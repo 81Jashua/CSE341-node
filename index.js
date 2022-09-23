b26771db706228128f6b49f5fcca90ef861f6a3d
Jashua <77981430+81Jashua@users.noreply.github.com>
2022-09-23 13:34:42 -0600

    Updating index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
 const app = express();
-const routes = require("./routes/index");
 
-app.use(routes);
+//const routes = require("./routes/index");
 
-app.listen(3000, () => {
-    console.log("application listening on http://localhost:3000");
-});
+//app.use(routes);
+// app.listen(3000, () => {
+//     console.log("application listening on http://localhost:3000");
+// });
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
